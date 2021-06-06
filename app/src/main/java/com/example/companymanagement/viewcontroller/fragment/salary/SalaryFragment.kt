759d14aa85a07ef1f627d14de2aa80691390fb66
JohnyHo-art <71172797+JohnyHo-art@users.noreply.github.com>
2021-06-06 22:58:19 +0700

    new update of salary screen
    - fix the salary with kotlin extension
    - disable the scalability of chart
    - clean code

--- app/src/main/java/com/example/companymanagement/viewcontroller/fragment/salary/SalaryFragment.kt
+++ app/src/main/java/com/example/companymanagement/viewcontroller/fragment/salary/SalaryFragment.kt
@@ -6,13 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.anychart.AnyChart
-import com.anychart.charts.Cartesian
 import com.example.companymanagement.R
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
-import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.XAxis
@@ -20,18 +14,9 @@ import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.formatter.ValueFormatter
-import com.github.mikephil.charting.utils.ColorTemplate
-import kotlinx.android.synthetic.main.fragment_navigation.*
 import kotlinx.android.synthetic.main.fragment_salary.*
 
 class SalaryFragment : Fragment() {
-    lateinit var barSalary: ArrayList<BarEntry>
-    lateinit var barDataSet: BarDataSet
-    lateinit var barData:BarData
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,54 +28,57 @@ class SalaryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        addData()
-    }
+        val salaryChart = view.findViewById<BarChart>(R.id.salary_chart)
+        val barSalary: ArrayList<BarEntry> = ArrayList()
+        val barData: BarData
 
-    fun addData(){
+        //Input bar entry
+        //Bar entry requires 2 values xAxis and yAxis
+        //2 values must be float
+        //xAxis must start from 0
+        barSalary.add(BarEntry(0f, 500f))
+        barSalary.add(BarEntry(1f, 300f))
+        barSalary.add(BarEntry(2f, 600f))
+        barSalary.add(BarEntry(3f, 200f))
+        barSalary.add(BarEntry(4f, 800f))
+        barSalary.add(BarEntry(5f, 900f))
+        barSalary.add(BarEntry(6f, 1000f))
+        barSalary.add(BarEntry(7f, 100f))
+        barSalary.add(BarEntry(8f, 1100f))
+        barSalary.add(BarEntry(9f, 1005f))
+        barSalary.add(BarEntry(10f, 500f))
+        barSalary.add(BarEntry(11f, 700f))
 
-        //set number of column bar and the value(x,y) of each bar
-        barSalary = ArrayList()
-        barSalary.add(BarEntry(0f,500f))
-        barSalary.add(BarEntry(1f,300f))
-        barSalary.add(BarEntry(2f,600f))
-        barSalary.add(BarEntry(3f,200f))
-        barSalary.add(BarEntry(4f,800f))
-        barSalary.add(BarEntry(5f,900f))
-        barSalary.add(BarEntry(6f,1000f))
-        barSalary.add(BarEntry(7f,100f))
-        barSalary.add(BarEntry(8f,1100f))
-        barSalary.add(BarEntry(9f,1005f))
-        barSalary.add(BarEntry(10f,500f))
-        barSalary.add(BarEntry(11f,000f))
-
-        //add month to the chart
-
-
-        barDataSet = BarDataSet(barSalary,"salary (dollar unit)")
-
+        //set data for the bar with barSalary list data above
+        //the label is used to describe the meaning of the bar column
+        val barDataSet = BarDataSet(barSalary, "salary (dollar unit)")
         barData = BarData(barDataSet)
         salary_chart.data = barData
 
+        //set some attributes for each bar
+        barDataSet.setColors(R.color.purple_200) //set the color of bars
+        barDataSet.valueTextColor = Color.BLACK //set color for the value text (ex: 500f)
+        barDataSet.valueTextSize = 10f //set text size for the value text
+        barDataSet.barBorderWidth = 0.5f //set border width for each bar
 
-        barDataSet.setColors(R.color.purple_200)
-        barDataSet.valueTextColor=Color.BLACK
-        barDataSet.valueTextSize=10f
-        barDataSet.barBorderWidth=0.5f
-
-        salary_chart.animateY(2000)
-        salary_chart.xAxis.valueFormatter = axisFormater()
-        salary_chart.xAxis.setDrawAxisLine(true)
-        salary_chart.xAxis.textSize=12f
-        salary_chart.xAxis.textColor = Color.RED
-        salary_chart.description=null
+        //set some attributes for the chart
+        salaryChart.animateY(2000) //set the animation for the yAxis (raise up in 0.2 second)
+        salaryChart.xAxis.valueFormatter = AxisFormater() //format the xAxis to string with a class
+        salaryChart.xAxis.textSize = 12f //set text size of xAxis label
+        salaryChart.xAxis.textColor = Color.RED //set text color
+        salaryChart.xAxis.labelRotationAngle = 35f //set rotation for xAxis label
+        salaryChart.xAxis.position = XAxis.XAxisPosition.BOTTOM //Move xAxis label to bottom
+        salaryChart.description = null //turn off the useless label description
+        salaryChart.setScaleEnabled(false) //disable scalability
     }
 
-}
-
-class axisFormater:ValueFormatter(){
-    private val months = arrayOf("Jan", "Feb", "Mar",
-    "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec")
-    override fun getAxisLabel(value: Float, axis: AxisBase?): String {
-        return months.getOrNull(value.toInt()) ?: value.toString()
+    //This class is used to reformat the xAxis label from float to string
+    class AxisFormater : ValueFormatter() {
+        private val months = arrayOf("Jan", "Feb", "Mar",
+            "Apr", "May", "June", "July", "Aug",
+            "Sep", "Oct", "Nov", "Dec")
+        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+            return months.getOrNull(value.toInt()) ?: value.toString()
+        }
     }
 }
