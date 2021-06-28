d0ed0635b7838bcd838d3cfebd4c3d41ae2f619d
Nqunhth <80096230+Nqunhth@users.noreply.github.com>
2021-06-28 19:27:35 +0700

    Full Employee Manageament + Full ForgotPass + UI TaskManagement

--- app/src/main/java/com/example/companymanagement/viewcontroller/fragment/task/TaskManager.kt
+++ app/src/main/java/com/example/companymanagement/viewcontroller/fragment/task/TaskManager.kt
@@ -1,4 +1,4 @@
-package com.example.companymanagement.viewcontroller.fragment.employe
+package com.example.companymanagement.viewcontroller.fragment.task
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,24 +8,22 @@ import android.view.ViewGroup
 import androidx.viewpager2.widget.ViewPager2
 import com.example.companymanagement.R
 import com.example.companymanagement.viewcontroller.adapter.EmployeViewPagerApdapter
+import com.example.companymanagement.viewcontroller.adapter.TaskViewPagerAdapter
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-class EmployeManager : Fragment() {
-
+class TaskManager : Fragment(){
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        val root = inflater.inflate(R.layout.fragment_tablayout_employeview, container, false)
-        val pager = root.findViewById<ViewPager2>(R.id.employee_manager_viewpager)
-        val tablayout = root.findViewById<TabLayout>(R.id.employee_manager_tab_layout)
-        pager.adapter = EmployeViewPagerApdapter(this);
-        var tabtext = resources.getStringArray(R.array.employee_manager_tab_name);
+        val root = inflater.inflate(R.layout.fragment_tablayout_taskview, container, false)
+        val pager = root.findViewById<ViewPager2>(R.id.task_manager_viewpager)
+        val tablayout = root.findViewById<TabLayout>(R.id.task_manager_tab_layout)
+        pager.adapter = TaskViewPagerAdapter(this);
+        var tabtext = resources.getStringArray(R.array.task_manager_tab_name);
         TabLayoutMediator(tablayout, pager) { tab, pos -> tab.text = tabtext[pos] }.attach()
         return root;
     }
-
-
 }
