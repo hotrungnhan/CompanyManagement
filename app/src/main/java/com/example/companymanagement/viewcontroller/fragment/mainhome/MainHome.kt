d24b6bc65b91e676984f3990a6b033bf138f9651
hotrungnhan <hotrungnhan29@gmail.com>
2021-07-04 19:09:03 +0700

    add icon

--- app/src/main/java/com/example/companymanagement/viewcontroller/fragment/mainhome/MainHome.kt
+++ app/src/main/java/com/example/companymanagement/viewcontroller/fragment/mainhome/MainHome.kt
@@ -56,11 +56,11 @@ class MainHome : Fragment() {
 
 
     fun createdata() {
-        adminlist = mutableListOf(HomeGridViewViewModel(R.drawable.ic_manager_user,
+        adminlist = mutableListOf(HomeGridViewViewModel(R.drawable.ic_user_manager,
             R.id.employee_manager,
             "Quản lý nhân viên", Color.valueOf(Color.CYAN)),
             HomeGridViewViewModel(
-                R.drawable.ic_task,
+                R.drawable.ic_clipboard,
                 R.id.task_manager,
                 "Quản lý task", Color.valueOf(Color.RED)),
             HomeGridViewViewModel(R.drawable.ic_stamp,
@@ -71,13 +71,13 @@ class MainHome : Fragment() {
                 "Quản lý lương"))
 
         userlist = mutableListOf(
-            HomeGridViewViewModel(R.drawable.ic_checking,
+            HomeGridViewViewModel(R.drawable.ic_qr_code,
                 R.id.checkin_qrscanner,
                 "Checking", Color.valueOf(Color.BLUE)),
             HomeGridViewViewModel(R.drawable.ic_salary_manager,
                 R.id.user_salary,
                 "Lương của tôi", Color.valueOf(Color.YELLOW), Color.valueOf(Color.DKGRAY)),
-            HomeGridViewViewModel(R.drawable.ic_ranking,
+            HomeGridViewViewModel(R.drawable.ic_statistics,
                 R.id.leaderboard,
                 "Bảng xếp hạng", Color.valueOf(Color.DKGRAY)),
             HomeGridViewViewModel(R.drawable.ic_absent,
