3153ccb96e9167f3a6b77551db7210c23c70e257
Nqunhth <80096230+Nqunhth@users.noreply.github.com>
2021-07-08 17:40:21 +0700

    Update UI, Employee Management, Task Management, Absence Management

--- app/src/main/java/com/example/companymanagement/viewcontroller/fragment/task/TaskOverall.kt
+++ app/src/main/java/com/example/companymanagement/viewcontroller/fragment/task/TaskOverall.kt
@@ -9,12 +9,16 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.companymanagement.R
+import java.text.SimpleDateFormat
 import java.time.Year
 import java.util.*
 
 class TaskOverall : Fragment() {
     private lateinit var taskviewmodel: TaskViewModel
-    var cal = Calendar.getInstance()
+    var cal = Calendar.getInstance().time
+    val month = SimpleDateFormat("MM", Locale.getDefault())
+    val year = SimpleDateFormat("yyyy", Locale.getDefault())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         taskviewmodel = ViewModelProvider(this.requireActivity()).get(TaskViewModel::class.java)
@@ -38,5 +42,12 @@ class TaskOverall : Fragment() {
         }
         return root
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val title = view.findViewById<TextView>(R.id.tv_task_title)
+        title.text = "Thống kê công việc tháng " + month.format(cal).toString() + " / " + year.format(cal)
+            .toString()
     }
 }
